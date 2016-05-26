f738fb4cfa16a8af7c425da83855830ede495022
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-05-27 03:00:21 +0700

    Added MapFileConverter. hbaseimport no longer have a main class

---
+++ hbaseimport/src/main/java/th/in/whs/ku/webapp/hbase/MapFileConverter.java
@@ -0,0 +1,30 @@
+package th.in.whs.ku.webapp.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MapFileOutputFormat;
+
+import java.io.IOException;
+
+public class MapFileConverter {
+    public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        conf.set("mapreduce.compress.map.output", "true");
+        conf.set("textinputformat.record.delimiter", "1q2w3e");
+        Job job = Job.getInstance(conf, "MapFileConvert");
+        job.setJarByClass(MapFileConverter.class);
+
+        job.setMapperClass(ParagraphMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(MapWritable.class);
+        TextInputFormat.setInputPaths(job, new Path(args[0]));
+        MapFileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
